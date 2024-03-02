07a6b815affb08378ca2384ea3360fce36e3a7d1
Gyasilarbi <gyasilarbi459@gmail.com>
2024-03-02 20:52:52 +0000

    update

--- schedule.php
+++ schedule.php
@@ -37,9 +37,17 @@
             padding: 50px;
         }
 
-        #underline {
-            color: lime;
-            height: 30px;
+        .day{
+            margin-bottom: 30px;
+        }
+
+        .day h2{
+            color: #1bbca3;
+        }
+
+        .day h6{
+            color: #1bbca3;
+            font-size: 15px;
         }
     </style>
 
@@ -78,11 +86,11 @@
         <section class="schedule">
             <div class="container">
                 <div class="day">
-                    <h2>DAY 0 (February 21 or March 6)</h2>
-                    <hr id="underline">
+                    <h2>DAY 0 (March 26)</h2>
+                    <hr id="underline" style="height: 2px; background-color: black;">
                     <div class="row">
                         <div class="col-lg-4">
-                            <h6 style="color: black;"> <i class="bi bi-clock"></i> 4:30pm - 6:30pm</h6>
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 4:30pm - 6:30pm</p>
                         </div>
                         <div class="col-lg-8">
                             <div class="card">
@@ -93,7 +101,101 @@
                                 </div>
                                 <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
                                     <div class="card-body">
-                                        <h6 style="color: black;">Instructions</h6>
+                                        <h6>Instructions</h6>
+                                        <p>Each school delegation will be handed out a shopper containing personal badges, placards, certificates of participation and conference handbooks.
+                                            If you cannot make it for registration, no worries you will find what you need waiting for you in your committee room, just follow instructions on monitors to find your room and ask one of your committee directors for your material.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="day">
+                    <h2>DAY 1 (March 27)</h2>
+                    <hr id="underline" style="height: 2px; background-color: black;">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 10:00am - 11:00am</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Rules of Procedure & Topic Briefing</h6>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 11:00am - 1:00pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Committee Session 1</h6>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 1:00pm - 2:20pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Lunch Break</h6>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 2:30pm - 4:30pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Committee Session 2</h6>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 4:30pm - 5:00pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Evening Break</h6>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 5:00pm - 6:30pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <h6>Committee Session 3</h6>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="day">
+                    <h2>DAY 2 (March 28)</h2>
+                    <hr id="underline" style="height: 2px; background-color: black;">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p style="color: black;"> <i class="bi bi-clock"></i> 4:30pm - 6:30pm</p>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    <a class="btn" data-bs-toggle="collapse" href="#collapseOne">
+                                        Participants Leave
+                                    </a>
+                                </div>
+                                <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
+                                    <div class="card-body">
+                                        <h6>Instructions</h6>
                                         <p>Each school delegation will be handed out a shopper containing personal badges, placards, certificates of participation and conference handbooks.
                                             If you cannot make it for registration, no worries you will find what you need waiting for you in your committee room, just follow instructions on monitors to find your room and ask one of your committee directors for your material.</p>
                                     </div>
@@ -105,6 +207,88 @@
             </div>
         </section>
     </main>
+
+        <!-- ======= Footer ======= -->
+        <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-info">
+                        <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-info">
+                        <p>
+                            Akroma Road <br>
+                            North Kaneshie, Accra- Ghana<br><br>
+                            <strong>Phone:</strong> +233 24 300 9448<br>
+                            <strong>Email:</strong> gyasilarbi459@gmail.com<br>
+                        </p>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Our Services</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Register Here</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Fees & Timeline</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Stay Updated with SSMUN</h4>
+                        <p>
+                            "Step into the world of diplomacy with SSMUN! <br> 🌍 Dive into engaging debates, broaden your understanding of global issues, and foster meaningful connections.
+                            <br> 📚 Discover SSMUN: Join us in our mission to empower the next generation of global leaders through immersive Model UN experiences.
+                            <br> 🌟 Empowering Experiences: SSMUN offers tailored experiences to nurture your diplomatic skills and broaden your horizons.
+                            <br> 🤝 Collaborative Community: Connect with like-minded individuals, share insights, and work together towards innovative solutions.
+                            <br> 📣 Join the Conversation: Get involved with SSMUN today, and let's shape a brighter future together!"
+                        </p>
+                        <form action="" method="post">
+                            <input type="email" name="email" placeholder="Your Email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>SSMUN</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> & <a href="">GyasiLarbi</a>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 </body>
 
 </html>
